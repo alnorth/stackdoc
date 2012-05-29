3b90ecc40c9d9c46d06c7ee75d648e0ac5998a6b
Alasdair North <alnorth@gmail.com>
2012-05-29 08:18:16 +0100

    Parsing datadumps almost there.

--- data-dump-parser/parser.py
+++ data-dump-parser/parser.py
@@ -1,16 +1,34 @@
+from pymongo import Connection
 import sys
-
 from xml.sax import make_parser, handler
 
 class SOProcessor(handler.ContentHandler):
 
     def __init__(self):
+        connection = Connection()
+        self._db = connection.stack_doc
+        self._posts = self._db.posts
         self._count = 0
 
     def startElement(self, name, attrs):
         if name == "row":
             if attrs["PostTypeId"] == "1":
                 if "<.net>" in attrs["Tags"] and "http://msdn.microsoft.com/" in attrs["Body"]:
+                    matches = re.findall(r"http://msdn\.microsoft\.com/en\-us/library/([a-zA-Z0-9\.]+)(_[a-z]+)?\.aspx", attrs["Body"])
+                    ids = []
+                    for match_tuple in matches:
+                        ids.append(match_tuple[0])
+                        print match_tuple[0]
+                    post = {
+                        "page_ids": ids,
+                        "id": attrs["Id"],
+                        "url": "http://stackoverflow.com/questions/%s" % attrs["Id"],
+                        "title": attrs["Title"],
+                        "score": attrs["Score"],
+                        "answers": attrs["AnswerCount"],
+                        "accepted_answer": "AcceptedAnswerId" in attrs
+                    }
+                    #self._posts.insert(post)
                     self._count += 1
                     print self._count
             
--- data-dump-parser/run-parser.sh
+++ data-dump-parser/run-parser.sh
@@ -3,3 +3,4 @@
 virtualenv --no-site-packages venv
 venv/bin/pip install -r requirements.txt
 venv/bin/python parser.py $1
+rm -rf venv
