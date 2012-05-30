e640c98ae8bf1a527b073d71fb4896cad095b290
Alasdair North <alnorth@gmail.com>
2012-05-30 08:19:11 +0100

    Attempting to fetch from SO.

--- update-database/update-latest-from-so.py
+++ update-database/update-latest-from-so.py
@@ -1,4 +1,10 @@
 import pymongo
+import stackexchange
+import time
+
+so = stackexchange.Site(stackexchange.StackOverflow, "WNe2LOp*hdsbD5U7kp0bhg((")
+
+so.be_inclusive()
 
 connection = pymongo.Connection()
 db = connection.stack_doc
@@ -8,4 +14,7 @@ up_to_date = False
 
 while not up_to_date:
     last_in_database = posts.find_one(sort=[("last_activity", pymongo.DESCENDING)])["last_activity"]
-    print "Fetching questions active after " + last_in_database
+    last_in_database_as_unix = int(time.mktime(last_in_database.timetuple()))
+    print "Fetching questions active after %s" % last_in_database_as_unix
+    rq = so.recent_questions(from_date=last_in_database_as_unix)
+    print rq[0]
