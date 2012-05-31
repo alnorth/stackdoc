eb267c1fd5cd901c0b40ccf2f18f429c10209e34
Alasdair North <alnorth@gmail.com>
2012-05-31 07:52:27 +0100

    Add SO update script to bash script.

--- update-database/update-database-and-write-files.sh
+++ update-database/update-database-and-write-files.sh
@@ -3,6 +3,8 @@
 virtualenv --no-site-packages venv
 venv/bin/pip install -r requirements.txt
 
+# Get the latest question updates from Stack Overflow
+venv/bin/python update-latest-from-so.py
 
 # Write out the files from the database
 venv/bin/python write-files.py
