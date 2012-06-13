b68dfdfd8b4ee2909657a09f1c5686b3652e992c
Alasdair North <alasdair@isfluent.com>
2012-06-13 16:41:19 +0100

    Actual ID updating from MSDN IDs.

--- update-database/map-msdn-ids.py
+++ update-database/map-msdn-ids.py
@@ -7,8 +7,16 @@ connection = Connection()
 db = connection.stack_doc
 posts = db.posts
 
-for id in posts.distinct("page_ids"):
+i = 1
+ids = posts.distinct("page_ids")
+
+for id in ids:
     if re.search("^[a-zA-Z0-9]{8}$", id):
         handle = urllib.urlopen("http://msdnid.alnorth.com/" + id)
-        print "%s - %s" % (id, handle.readline())
+        if handle.getcode() == 200:
+            canonical = handle.readline()
+            print "%i/%i - %s - %s" % (i, len(ids), id, canonical)
+            posts.update({"page_ids": id}, {"$addToSet": {"page_ids": canonical}}, multi=True)
+            posts.update({"page_ids": id}, {"$pull": {"page_ids": id}}, multi=True)
+    i += 1
 
