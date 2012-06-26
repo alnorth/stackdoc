5cfb9abc2824465c4941b771e347d5c54c77ed7c
Alasdair North <alnorth@gmail.com>
2012-06-26 08:19:26 +0100

    Remove file writing step. No longer needed as we use a node.js server.

--- update-database/update-database-and-write-files.sh
+++ update-database/update-database-and-write-files.sh
@@ -6,7 +6,4 @@ venv/bin/pip install -r requirements.txt
 # Get the latest question updates from Stack Overflow
 venv/bin/python update-latest-from-so.py
 
-# Write out the files from the database
-venv/bin/python write-files.py
-
 rm -rf venv
