2bca0a22ed4b35d533144e91b414182d77538d1f
Alasdair North <alnorth@gmail.com>
2012-06-29 23:15:29 +0100

    Only update a question if it's with new data.

--- update-database/stackdoc/questionimport.py
+++ update-database/stackdoc/questionimport.py
@@ -11,23 +11,29 @@ def import_question(posts, languages, id, title, body, tags, last_activity_date,
     if len(namespaces):
         post = posts.find_one({"question_id": int(id)})
         previously_existed = False
+        update = True
         if post:
             previously_existed = True
+            # Only update title, score etc. if this is the latest data
+            update = post["last_activity"] < last_activity_date
         else:
-            post = {}
+            post = {
+                "question_id": int(id),
+                "url": "http://stackoverflow.com/questions/%s" % id
+            }
 
         post["namespaces"] = namespaces
-        post["question_id"] = int(id)
-        post["url"] = "http://stackoverflow.com/questions/%s" % id
-        post["title"] = title
-        post["score"] = int(score)
-        post["answers"] = int(answer_count)
-        post["accepted_answer"] = has_accepted_answer
-        post["last_activity"] = last_activity_date
+        if update:
+            post["title"] = title
+            post["score"] = int(score)
+            post["answers"] = int(answer_count)
+            post["accepted_answer"] = has_accepted_answer
+            post["last_activity"] = last_activity_date
 
         if previously_existed:
             posts.update({"question_id": int(id)}, post)
         else:
             posts.insert(post)
 
-        print "%s %s question from %s (%s)" % ("Updated" if previously_existed else "Inserted", ", ".join(namespaces.keys()), str(last_activity_date), id)
+        update_text = "Fully updated" if update else "Partially updated"
+        print "%s %s question from %s (%s)" % (update_text if previously_existed else "Inserted", ", ".join(namespaces.keys()), str(last_activity_date), id)
