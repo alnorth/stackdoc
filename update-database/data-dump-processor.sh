e3c973700a9a8e9518e931c0e7253b617bf830e9
Alasdair North <alnorth@gmail.com>
2012-06-29 23:21:34 +0100

    Remove old update scripts.

--- update-database/data-dump-processor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-virtualenv --no-site-packages data-dump-processor-venv
-data-dump-processor-venv/bin/pip install -r data-dump-processor-requirements.txt
-data-dump-processor-venv/bin/python data-dump-processor.py $1
-rm -rf data-dump-processor-venv
