127bdea292e638e0d808a7682b579b1fea007e8c
Alasdair North <alnorth@gmail.com>
2012-07-03 08:04:12 +0100

    Ensure all files are indented with spaces.

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -1,34 +1,34 @@
 var stackdoc = (function() {
-	var module = {};
+    var module = {};
 
-	var sdTemplate = '' +
-		'<table class="stackdoc">' +
-		'{{#questions}}' +
-		'	<tr>' +
-		'		<td><span class="sd_score {{#accepted_answer}}sd_accepted{{/accepted_answer}}">{{score}}</span></td>' +
-		'		<td class="stackdoc-question-title"><a href="{{url}}" title="{{answers}} answers{{#accepted_answer}}, with one accepted answer{{/accepted_answer}}">{{title}}</a></td>' +
-		'	</tr>' +
-		'{{/questions}}' +
-		'</table>';
+    var sdTemplate = '' +
+        '<table class="stackdoc">' +
+        '{{#questions}}' +
+        '   <tr>' +
+        '       <td><span class="sd_score {{#accepted_answer}}sd_accepted{{/accepted_answer}}">{{score}}</span></td>' +
+        '       <td class="stackdoc-question-title"><a href="{{url}}" title="{{answers}} answers{{#accepted_answer}}, with one accepted answer{{/accepted_answer}}">{{title}}</a></td>' +
+        '   </tr>' +
+        '{{/questions}}' +
+        '</table>';
 
-	function url(namespace, id) {
-		return "http://stackdocapi.alnorth.com/1/" + encodeURIComponent(namespace) + "/" + encodeURIComponent(id);
-	}
+    function url(namespace, id) {
+        return "http://stackdocapi.alnorth.com/1/" + encodeURIComponent(namespace) + "/" + encodeURIComponent(id);
+    }
 
-	function compareQuestions(a, b) {
-		return b.score - a.score;
-	}
+    function compareQuestions(a, b) {
+        return b.score - a.score;
+    }
 
-	module.fetchData = function(namespace, id, callback) {
-		if(id) {
-			$.getJSON(url(namespace, id), function(data) {
-				data.sort(compareQuestions);
-				var renderedList = Mustache.render(sdTemplate, {questions: data});
+    module.fetchData = function(namespace, id, callback) {
+        if(id) {
+            $.getJSON(url(namespace, id), function(data) {
+                data.sort(compareQuestions);
+                var renderedList = Mustache.render(sdTemplate, {questions: data});
 
-				callback(data, renderedList);
-			});
-		}
-	}
+                callback(data, renderedList);
+            });
+        }
+    }
 
-	return module;
+    return module;
 }());
