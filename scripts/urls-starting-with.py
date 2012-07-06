c0a4dfc5730f84b171feec276aaf2a95faefa18a
Alasdair North <alnorth@gmail.com>
2012-07-06 08:04:34 +0100

    A script for finding all the URLs in a data dump that start with a particular prefix. Useful when designing regexs to process URLs for a namespace.

---
+++ scripts/urls-starting-with.py
@@ -0,0 +1,23 @@
+import re
+import sys
+from xml.sax import make_parser, handler
+
+if len(sys.argv) < 3:
+    print "This script expects two arguments: \n1. The path to a posts.xml file from a Stack Overflow data dump.\n2. A URL prefix to search for."
+else:
+    start_with = sys.argv[2]
+
+    class SOProcessor(handler.ContentHandler):
+
+        def startElement(self, name, attrs):
+            if name == "row":
+                if attrs["PostTypeId"] == "1":
+                    body = attrs["Body"]
+                    if start_with in body:
+                        matches =  re.findall(r'<a href="([^"]+)"', body)
+                        for url in filter(lambda x: x.startswith(start_with), matches):
+                            print url
+
+    parser = make_parser()
+    parser.setContentHandler(SOProcessor())
+    parser.parse(open(sys.argv[1]))
