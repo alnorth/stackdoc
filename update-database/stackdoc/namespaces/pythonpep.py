20fe6a4c6eda6e6e9f40c559100015d69a25a73f
Alasdair North <alnorth@gmail.com>
2012-07-08 11:04:36 +0100

    Add pythonpep parser.

---
+++ update-database/stackdoc/namespaces/pythonpep.py
@@ -0,0 +1,23 @@
+import re
+import urllib
+
+
+############### Functions called by stackdoc
+
+def get_version():
+    return 1
+
+def get_ids(title, body, tags):
+    ids = []
+    if "http://docs.python.org/" in body:
+        urls = re.findall(r'<a href="([^"]+)"', body)
+        for url in urls:
+            m = re.match("http:\/\/www\.python\.org\/dev\/peps\/pep\-([0-9]+)", url)
+            if m:
+                ids.append(m.group(1))
+    return ids
+
+def get_tags():
+    return [
+        "python"
+    ]
