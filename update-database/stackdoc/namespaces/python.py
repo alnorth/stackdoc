29316060fb422a881833e411350e0149575bf1c4
Alasdair North <alasdair@isfluent.com>
2012-07-09 12:57:03 +0100

    Support old style Python doc links.

--- update-database/stackdoc/namespaces/python.py
+++ update-database/stackdoc/namespaces/python.py
@@ -5,16 +5,20 @@ import urllib
 ############### Functions called by stackdoc
 
 def get_version():
-    return 1
+    return 2
 
 def get_ids(title, body, tags):
     ids = []
-    if "http://docs.python.org/" in body:
+    if "http://docs.python.org/" in body or "http://www.python.org/doc/" in body:
         urls = re.findall(r'<a href="([^"]+)"', body)
         for url in urls:
-            m = re.match("http://docs.python.org/(?:release/)?(?:dev/)?(?:[0-9](?:\.[0-9]/)+)?(?:py3k/)?library/([.a-z0-9]+)(?:-examples)?\.html", url)
-            if m:
-                ids.append(m.group(1))
+            docsm = re.match("http://docs.python.org/(?:release/)?(?:dev/)?(?:[0-9](?:\.[0-9]/)+)?(?:py3k/)?library/([.a-z0-9]+)(?:-examples)?\.html", url)
+            if docsm:
+                ids.append(docsm.group(1))
+            olddocsm = re.match("http://www.python.org/doc/(?:[0-9](?:\.[0-9]/)+)/lib/module-([.a-z0-9]+)\.html", url)
+            if olddocsm:
+                ids.append(olddocsm.group(1))
+
     return ids
 
 def get_tags():
