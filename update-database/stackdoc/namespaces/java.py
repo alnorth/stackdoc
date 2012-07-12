6fb981d7d0a8bd2c4f0328dfac79c5cb3fb80b8e
Alasdair North <alnorth@gmail.com>
2012-07-12 07:49:29 +0100

    First go at a Java parser.

---
+++ update-database/stackdoc/namespaces/java.py
@@ -0,0 +1,24 @@
+import re
+import urllib
+
+
+############### Functions called by stackdoc
+
+def get_version():
+    return 1
+
+def get_ids(title, body, tags):
+    ids = []
+    if "http://java.sun.com/" in body or "http://docs.oracle.com/":
+        urls = re.findall(r'<a href="([^"]+)"', body)
+        for url in urls:
+            m = re.match("http://(?:java\.sun|docs\.oracle)\.com/(?:javase|j2se)/\d(?:\.\d)*/docs/api/(\w+(?:/\w+)*)\.html", url)
+            if m:
+                ids.append(m.group(1).replace("/", "."))
+
+    return ids
+
+def get_tags():
+    return [
+        "java"
+    ]
